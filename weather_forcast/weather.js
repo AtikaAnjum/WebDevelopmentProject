d9cec060652653da42e6cdc6323268ee38254f1f
AtikaAnjum <atikaanjum008@gmail.com>
2024-07-01 01:29:03 -0700

    weather.js

--- weather_forcast/weather.js
+++ weather_forcast/weather.js
@@ -5,10 +5,10 @@ const temperatureDisplay = document.getElementById("temperature");
 const humidityDisplay = document.getElementById("humidity");
 const windSpeedDisplay = document.getElementById("wind-speed");
 const weatherDisplay = document.getElementById("weather");
-// const temperatureLoading = document.getElementById("temperature-loading");
-// const humidityLoading = document.getElementById("humidity-loading");
-// const windSpeedLoading = document.getElementById("wind-speed-loading");
-// const weatherLoading = document.getElementById("weather-loading");
+const temperatureLoading = document.getElementById("temperature-loading");
+const humidityLoading = document.getElementById("humidity-loading");
+const windSpeedLoading = document.getElementById("wind-speed-loading");
+const weatherLoading = document.getElementById("weather-loading");
 const cityNameHeader = document.getElementById("city-name-header");
 const weatherImage = document.getElementById("weather-image");
 
@@ -16,15 +16,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const fetchWeather = async (location) => {
         try {
-            // temperatureDisplay.textContent = ""; // Clear previous content
-            // humidityDisplay.textContent = "";
-            // windSpeedDisplay.textContent = "";
-            // weatherDisplay.textContent = "";
+            temperatureDisplay.textContent = ""; // Clear previous content
+            humidityDisplay.textContent = "";
+            windSpeedDisplay.textContent = "";
+            weatherDisplay.textContent = "";
 
-            // temperatureLoading.style.display = "block"; // Display loading indicators
-            // humidityLoading.style.display = "block";
-            // windSpeedLoading.style.display = "block";
-            // weatherLoading.style.display = "block";
+            temperatureLoading.style.display = "block"; // Display loading indicators
+            humidityLoading.style.display = "block";
+            windSpeedLoading.style.display = "block";
+            weatherLoading.style.display = "block";
 
             const apiKey = '3311e16227c34165492acc31a63d8a05';
             const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric`;
@@ -35,10 +35,10 @@ document.addEventListener("DOMContentLoaded", function () {
                 throw new Error(data.message || "City not found");
             }
 
-            // temperatureLoading.style.display = "none"; // Hide loading indicators
-            // humidityLoading.style.display = "none";
-            // windSpeedLoading.style.display = "none";
-            // weatherLoading.style.display = "none";
+            temperatureLoading.style.display = "none"; // Hide loading indicators
+            humidityLoading.style.display = "none";
+            windSpeedLoading.style.display = "none";
+            weatherLoading.style.display = "none";
 
             temperatureDisplay.textContent = data.main.temp + "°C";
             humidityDisplay.textContent = data.main.humidity + "%";
@@ -52,10 +52,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
         } catch (error) {
             console.error("Error fetching weather data:", error);
-            // temperatureLoading.style.display = "none"; // Hide loading indicators
-            // humidityLoading.style.display = "none";
-            // windSpeedLoading.style.display = "none";
-            // weatherLoading.style.display = "none";
+            temperatureLoading.style.display = "none"; // Hide loading indicators
+            humidityLoading.style.display = "none";
+            windSpeedLoading.style.display = "none";
+            weatherLoading.style.display = "none";
 
             temperatureDisplay.textContent = "N/A";
             humidityDisplay.textContent = "N/A";
@@ -68,28 +68,28 @@ document.addEventListener("DOMContentLoaded", function () {
     };
 
     // Function to update weather image based on weather condition
-    // const updateWeatherImage = (weatherCondition) => {
-    //     const imageMappings = {
-    //         "Clear": "images/sunny.jpg",
-    //         "Clouds": "images/cloudy.jpg",
-    //         "Rain": "images/rain.jpg",
-    //         "Drizzle": "images/rain.jpg",
-    //         "Thunderstorm": "images/thunder.jpg",
-    //         "Mist": "images/fog(2).jpg",
-    //         "Fog": "images/fog(2).jpg",
-    //         "Smoke": "images/smoke.jpg",
-    //         "Haze": "images/haze.jpg",
-    //         "Dust": "images/dust.jpg",
+    const updateWeatherImage = (weatherCondition) => {
+        const imageMappings = {
+            "Clear": "images/sunny.jpg",
+            "Clouds": "images/cloudy.jpg",
+            "Rain": "images/rain.jpg",
+            "Drizzle": "images/rain.jpg",
+            "Thunderstorm": "images/thunder.jpg",
+            "Mist": "images/fog(2).jpg",
+            "Fog": "images/fog(2).jpg",
+            "Smoke": "images/smoke.jpg",
+            "Haze": "images/haze.jpg",
+            "Dust": "images/dust.jpg",
             
-    //     };
+        };
  
-    //     const imageUrl = imageMappings[weatherCondition];
-    //     if (imageUrl) {
-    //         weatherImage.src = imageUrl;
-    //     } else {
-    //         weatherImage.src = "images/sunny.jpg";
-    //     }
-    // };
+        const imageUrl = imageMappings[weatherCondition];
+        if (imageUrl) {
+            weatherImage.src = imageUrl;
+        } else {
+            weatherImage.src = "images/sunny.jpg";
+        }
+    };
 
     if (searchForm) {
         // initially will display for london
